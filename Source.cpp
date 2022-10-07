a7f62000d9dcc21ce07601ddf588e14b48ba855c
Michał Makaruk <ul0253616@edu.uni.lodz.pl>
2022-10-07 08:42:34 +0200

    Main function and MySql config.

--- Source.cpp
+++ Source.cpp
@@ -2,3 +2,15 @@
 
 using namespace System;
 using namespace System :: Windows :: Forms;
+
+[STAThreadAttribute]
+
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	DatabasesAppsdoctorsOffice::Log_in form;
+	Application::Run(% form);
+
+	return 0;
+}
